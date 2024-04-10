dd1cac71c8e3698a7c83e8c234ff639730b51816
Horikaze <adrianyt659@gmail.com>
2024-04-10 19:18:12 +0200

    added footer, links

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
 import MainNavbar from './components/MainNavbar.vue'
+import { FaHeart, FaBandsVuejs, FaBandsGithub } from "@kalimahapps/vue-icons";
 </script>
 
 <template>
   <MainNavbar />
-  <main class="max-w-[1400px] mx-auto px-2 mt-20 mb-10">
+  <main class="max-w-[1400px] mx-auto px-2 mt-20 mb-5 min-h-screen">
     <RouterView />
   </main>
+  <footer class="flex flex-col gap-y-2 w-full items-center justify-center font-semibold mb-5 border-t-2 pt-5">
+    <div class="flex items-center justify-center gap-x-1">
+      <FaBandsGithub class="size-6" /> <a href="https://github.com/Horikaze/Vuestore" class="underline"
+        target="_blank">https://github.com/Horikaze/Vuestore</a>
+    </div>
+    <div class="flex items-center">Made with
+      <FaHeart class="text-red-500 mx-1 size-5" />by<a href="https://github.com/Horikaze" target="_blank"
+        class="underline ml-1">Horikaze</a>
+    </div>
+    <div class="flex items-center justify-center">Using
+      <FaBandsVuejs class="size-8" />
+    </div>
+  </footer>
 </template>
--- src/components/sub/LoadingState.vue
+++ src/components/sub/LoadingState.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
-import LoadingCard from './LoadingCard.vue';
 
-
-const myArray = [1, 2, 3, 4, 5]
 </script>
 
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-10">
-    <LoadingCard v-for="item in myArray" :key="item" />
+    <div v-for="i in 5" :key="i" class="w-full h-[420px] flex flex-col justify-center bg-white p-3 border-2 rounded-md">
+      <div class="h-[40px]  py-[10px]">
+        <div class="bg-gray-300 w-48 h-full animate-pulse rounded-md"></div>
+      </div>
+      <div class="w-full h-56 bg-gray-300 rounded-md"></div>
+      <div class="mt-auto w-full h-5 bg-gray-300 rounded-md"></div>
+      <div class="mt-2 w-2/3 self-start h-5 bg-gray-300 rounded-md"></div>
+      <div class="mt-4 text-xl w-full flex items-center h-[40px]">
+        <div class="bg-gray-300 rounded-md h-8 w-20"></div>
+      </div>
+    </div>
+
   </div>
 </template>
